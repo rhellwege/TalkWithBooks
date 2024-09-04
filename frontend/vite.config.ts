6d2a0d7f3dafe5cb34a3c60378c802aa18954bff
Ryan Hellwege <rhellwege6@gmail.com>
2024-09-04 10:45:53 -0700

    vite-react setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
